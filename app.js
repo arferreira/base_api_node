0d3412ef42cfeb0f16311ae00ed9b412bc52897e
Antonio Ricardo Ferreira de Souza <antonioricardoarfs@gmail.com>
2022-08-03 06:18:14 -0400

    configurate mongoose and other things to structure

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+require('./config/database');
 
-
-var usersRouter = require('./routes/users');
+var usersRouter = require('./app/routes/users');
 
 var app = express();
 
