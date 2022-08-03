3c3b02d64a1e3926b500da8bfbecb482d7e483ff
Antonio Ricardo Ferreira de Souza <antonioricardoarfs@gmail.com>
2022-08-03 08:53:20 -0400

    configure jest to tdd

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import dotenv from 'dotenv'
 dotenv.config()
-import express from 'express'
+import app from './app'
 import usersRouter from './routes/users'
-const app = express();
 
-app.use(express.json())
+
+
 
 
 app.use('/users', usersRouter)
 
 
-app.listen(process.env.API_PORT, () => {
-  console.info(`Running API on port ${process.env.API_PORT}`)
+
+const API_PORT = process.env.API_PORT || 3000
+
+app.listen(API_PORT, () => {
+  console.info(`Running API on port ${API_PORT}`)
 })
