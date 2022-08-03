245eaeb14fab3455c4ca10cb61c2eefc91de0616
Antonio Ricardo Ferreira de Souza <antonioricardoarfs@gmail.com>
2022-08-03 11:38:05 -0400

    configure swagger to documentate API

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -2,8 +2,12 @@ import express, { Express } from "express";
 import cors from "cors";
 import connectToMongoDb from "../../database";
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../../swagger.json';
+
 export default function appConfig(app: Express): void {
   connectToMongoDb()
   app.use(cors())
   app.use(express.json())
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 }
