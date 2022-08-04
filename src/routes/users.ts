92917f43914527e3dde2651cfcdeef9bc003a9f9
Antonio Ricardo Ferreira de Souza <antonioricardoarfs@gmail.com>
2022-08-04 14:50:11 -0400

    Comments routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,7 +5,7 @@ import { createUserController } from "src/useCases/CreateUser"
 const router = express.Router()
 
 
-// Routes to users
+// Routes to users register
 router.post('/register', (req, res) => {
   return createUserController.handle(req, res);
 })
